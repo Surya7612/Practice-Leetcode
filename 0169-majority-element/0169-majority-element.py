e0c9c0ef2db19bc35d80a70f14647b1ebb571f33
Surya Nediyadeth <49335157+Surya7612@users.noreply.github.com>
2024-05-17 04:01:25 -0400

    Update 0169-majority-element.py

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,6 +1,24 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
+    #counter class approach
+        # s = Counter(nums).most_common()
+        # return s[0][0]
 
-        s = Counter(nums).most_common()
+    #sorting approach
+        # nums.sort()
+        # n = len(nums)
+        # return nums[n//2]
 
-        return s[0][0]
+    #Boyer Moore's Voting Algo. approach
+        count = 0
+        maj_el = -1
+
+        for num in nums:
+            if count == 0:
+                maj_el = num
+            if num == maj_el:
+                count += 1
+            else:
+                count -= 1
+        
+        return maj_el
